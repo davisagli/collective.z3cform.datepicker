de23427d34042ec693231411d9cde1fcc9e6b6c6
Rok Garbas <rok.garbas@gmail.com>
2008-10-05 21:01:11 +0200

    description added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1b1'
 
 setup(name='collective.z3cform.datepicker',
       version=version,
-      description="",
+      description="calendar widget for z3c.form",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
